f762504c4320007a2462fc625d1aa56e0f8d8ece
jsantino <jsantino@kaber.cs.washington.edu>
2016-04-20 11:39:25 -0700

    Polymorphic Qualifier

---
+++ src/annotations/PolyIndex.java
@@ -0,0 +1,9 @@
+package annotations;
+
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import org.checkerframework.framework.qual.PolymorphicQualifier;
+
+@PolymorphicQualifier
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+public @interface PolyIndex { }
