29925cab379c1c07a1941e2752cca0d3558919d3
jsantino <jsantino@kaber.cs.washington.edu>
2016-04-20 12:36:11 -0700

    make indexTop implicitfFor int

--- src/index/qual/IndexTop.java
+++ src/index/qual/IndexTop.java
@@ -2,12 +2,16 @@ package index.qual;
 
 import java.lang.annotation.*;
 
+import javax.lang.model.type.TypeKind;
+
 import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
+import org.checkerframework.framework.qual.ImplicitFor;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 @SubtypeOf({})
 @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
 @DefaultQualifierInHierarchy
+@ImplicitFor(types = TypeKind.INT)
 public @interface IndexTop {
 
 }
