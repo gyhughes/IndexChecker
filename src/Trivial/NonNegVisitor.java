43cef61c17ee8cc85551eb0fb6166c2ea50b1cb0
jsantino <jsantino@agraf.cs.washington.edu>
2016-04-24 15:50:36 -0700

    finish trivial visitor, Type Rules are enforced(only access arrays with nonnegative)

---
+++ src/Trivial/NonNegVisitor.java
@@ -0,0 +1,30 @@
+package Trivial;
+
+import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
+import org.checkerframework.common.basetype.BaseTypeChecker;
+import org.checkerframework.common.basetype.BaseTypeVisitor;
+import org.checkerframework.framework.source.Result;
+import org.checkerframework.framework.type.AnnotatedTypeMirror;
+
+import com.sun.source.tree.ArrayAccessTree;
+import com.sun.source.tree.ExpressionTree;
+
+
+
+public class NonNegVisitor extends BaseTypeVisitor<BaseAnnotatedTypeFactory>{
+
+	public NonNegVisitor(BaseTypeChecker checker) {
+		super(checker);
+	}
+
+	@Override
+	public Void visitArrayAccess(ArrayAccessTree tree, Void type){
+		ExpressionTree index = tree.getIndex();
+		AnnotatedTypeMirror indexType = atypeFactory.getAnnotatedType(index);
+		if (!indexType.hasAnnotation(Trivial.qual.NonNegative.class)) {
+			checker.report(Result.warning("unsafe array access: only use NonNegative index"), index);
+		}
+		return super.visitArrayAccess(tree, type);
+	}
+
+}
