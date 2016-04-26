989cf1dbba8e4d525a7aa10dda42af0d2b5bd492
jsantino <jsantino@abelay.cs.washington.edu>
2016-04-26 16:34:17 -0700

    implement flow inference, x >= 0 -> @NonNegative x

--- src/Trivial/NonNegTransfer.java
+++ src/Trivial/NonNegTransfer.java
@@ -2,13 +2,22 @@ package Trivial;
 
 import javax.lang.model.element.AnnotationMirror;
 
+import org.checkerframework.dataflow.analysis.ConditionalTransferResult;
+import org.checkerframework.dataflow.analysis.FlowExpressions.LocalVariable;
+import org.checkerframework.dataflow.analysis.FlowExpressions.Receiver;
 import org.checkerframework.dataflow.analysis.RegularTransferResult;
 import org.checkerframework.dataflow.analysis.TransferInput;
 import org.checkerframework.dataflow.analysis.TransferResult;
+import org.checkerframework.dataflow.cfg.node.GreaterThanOrEqualNode;
+import org.checkerframework.dataflow.cfg.node.LocalVariableNode;
+import org.checkerframework.dataflow.cfg.node.Node;
 import org.checkerframework.dataflow.cfg.node.NumericalSubtractionNode;
 import org.checkerframework.framework.flow.CFAbstractTransfer;
 import org.checkerframework.framework.flow.CFStore;
 import org.checkerframework.framework.flow.CFValue;
+import org.checkerframework.framework.type.AnnotatedTypeMirror;
+
+import Trivial.qual.NonNegative;
 
 public class NonNegTransfer extends CFAbstractTransfer<CFValue, CFStore, NonNegTransfer> {
 
@@ -19,7 +28,7 @@ public class NonNegTransfer extends CFAbstractTransfer<CFValue, CFStore, NonNegT
 		super(analysis);
 		this.analysis = analysis;
 	}
-	
+
 	// make any subtraction result in an @unknown value.
 	@Override
 	public TransferResult<CFValue, CFStore> visitNumericalSubtraction(NumericalSubtractionNode n, TransferInput<CFValue, CFStore> in){
@@ -28,5 +37,38 @@ public class NonNegTransfer extends CFAbstractTransfer<CFValue, CFStore, NonNegT
 		AnnotationMirror anno = atypeFactory.createUnknownAnnotation();
 		CFValue newResultValue = analysis.createSingleAnnotationValue(anno, result.getResultValue().getType().getUnderlyingType());
 		return new RegularTransferResult<>(newResultValue, result.getRegularStore());
+	}
+
+
+	//checking >= @NonNegative transforms to @NonNegative
+	@Override
+	public TransferResult<CFValue, CFStore> visitGreaterThanOrEqual(GreaterThanOrEqualNode node, TransferInput<CFValue, CFStore> in){
+		Node right = node.getRightOperand();
+		NonNegAnnotatedTypeFactory atypeFactory = (NonNegAnnotatedTypeFactory) analysis.getTypeFactory();
+		TransferResult<CFValue, CFStore> result = super.visitGreaterThanOrEqual(node, in);
+		AnnotatedTypeMirror operand = atypeFactory.getAnnotatedType(right.getTree());	
+
+		CFStore thenStore = result.getRegularStore();
+		CFStore elseStore = thenStore.copy();
+
+		ConditionalTransferResult<CFValue, CFStore> newResult = new ConditionalTransferResult<>(result.getResultValue(), thenStore, elseStore);
+
+		try{
+			LocalVariableNode n =((LocalVariableNode) node.getLeftOperand());
+			Receiver r = new LocalVariable(n);
+			elseStore.insertValue(r , atypeFactory.createUnknownAnnotation());
+			if(operand.hasAnnotation(NonNegative.class)){
+			AnnotationMirror anno = atypeFactory.createNonNegAnnotation();
+			thenStore.insertValue(r, anno);
+			return newResult;
+			}
+		}
+		catch(ClassCastException e){
+		}
+		AnnotationMirror anno = atypeFactory.createUnknownAnnotation();
+		CFValue newResultValue = analysis.createSingleAnnotationValue(anno, result.getResultValue().getType().getUnderlyingType());
+		return new RegularTransferResult<>(newResultValue, result.getRegularStore());
+
+
 	}
 }
