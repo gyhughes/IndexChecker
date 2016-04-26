c68fa55bf23f73e29997374753fea651cbee2507
jsantino <jsantino@abelay.cs.washington.edu>
2016-04-26 14:21:01 -0700

    Add Subtraction Transfer rule for NonNegChecker
    Any combination of subtraction results in Unknown

--- src/Trivial/NonNegTransfer.java
+++ src/Trivial/NonNegTransfer.java
@@ -1,6 +1,11 @@
 package Trivial;
 
-import org.checkerframework.framework.flow.CFAbstractAnalysis;
+import javax.lang.model.element.AnnotationMirror;
+
+import org.checkerframework.dataflow.analysis.RegularTransferResult;
+import org.checkerframework.dataflow.analysis.TransferInput;
+import org.checkerframework.dataflow.analysis.TransferResult;
+import org.checkerframework.dataflow.cfg.node.NumericalSubtractionNode;
 import org.checkerframework.framework.flow.CFAbstractTransfer;
 import org.checkerframework.framework.flow.CFStore;
 import org.checkerframework.framework.flow.CFValue;
@@ -9,10 +14,19 @@ public class NonNegTransfer extends CFAbstractTransfer<CFValue, CFStore, NonNegT
 
 	/** like super.analysis, but more specific */
 	protected NonNegAnalysis analysis;
-	
+
 	public NonNegTransfer(NonNegAnalysis analysis) {
 		super(analysis);
 		this.analysis = analysis;
 	}
-
+	
+	// make any subtraction result in an @unknown value.
+	@Override
+	public TransferResult<CFValue, CFStore> visitNumericalSubtraction(NumericalSubtractionNode n, TransferInput<CFValue, CFStore> in){
+		NonNegAnnotatedTypeFactory atypeFactory = (NonNegAnnotatedTypeFactory) analysis.getTypeFactory();
+		TransferResult<CFValue, CFStore> result = super.visitNumericalSubtraction(n, in);
+		AnnotationMirror anno = atypeFactory.createUnknownAnnotation();
+		CFValue newResultValue = analysis.createSingleAnnotationValue(anno, result.getResultValue().getType().getUnderlyingType());
+		return new RegularTransferResult<>(newResultValue, result.getRegularStore());
+	}
 }
