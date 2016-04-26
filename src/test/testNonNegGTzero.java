022dce8693f097568be846d289229868aa15b740
jsantino <jsantino@abelay.cs.washington.edu>
2016-04-26 14:25:05 -0700

    add test file for the data flow of NonNeg

---
+++ src/test/testNonNegGTzero.java
@@ -0,0 +1,22 @@
+package test;
+
+import Trivial.qual.*;
+
+public class testNonNegGTzero {
+
+	public static void main(String[] args) {
+		test(2);
+	}
+	
+	public static void test(@Unknown int x){
+		int[] a = new int[6];
+		if(x >= 0){
+			int c = a[x];
+		}
+		if(x > -1){
+			int c = a[x];
+		}
+		
+	}
+
+}
