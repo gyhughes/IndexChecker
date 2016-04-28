48956920a301775fddf53f94c487abaddc1d8683
Kevin Klein <kleinkevin.lu@gmail.com>
2016-04-27 17:13:34 -0700

    adapted use of nnotation class to new, refactored name

--- src/test/testProgram.java
+++ src/test/testProgram.java
@@ -6,7 +6,7 @@ public class testProgram {
 	public static void main(String[] args) {
 		int[] a = new int[47];
 		@IndexTop int sum = 0;
-		@IndexHigh("a") int l = a.length;
+		@IndexOrHigh("a") int l = a.length;
 		for (@IndexFor("a") int i = 0; i < l; i++) {
 			sum += a[i];
 		}
